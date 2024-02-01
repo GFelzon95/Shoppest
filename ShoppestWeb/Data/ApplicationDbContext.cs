9ba8e35c49030208716aae66f3092bb3f875d853
Gian Felzon Pascual <pascual.gianfelzon@wesleyan.edu.ph>
2024-02-01 22:57:23 +0800

    -Created ProductCategory Model -Created and Seeded ProductCategory Table in the Database

--- ShoppestWeb/Data/ApplicationDbContext.cs
+++ ShoppestWeb/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using ShoppestWeb.Models;
 
 namespace ShoppestWeb.Data
 {
@@ -7,6 +8,18 @@ namespace ShoppestWeb.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
 
+        }
+
+        DbSet<ProductCategory> ProductCategories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ProductCategory>().HasData(
+                new ProductCategory { Id = 1, Name = "Fashion" },
+                new ProductCategory { Id = 2, Name = "Electronics" },
+                new ProductCategory { Id = 3, Name = "Toys" },
+                new ProductCategory { Id = 4, Name = "Sports" }
+                );
         }
     }
 }
---
+++ ShoppestWeb/Migrations/20240201145457_SeedProductCategoryTable.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace ShoppestWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedProductCategoryTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "ProductCategories",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Fashion" },
+                    { 2, "Electronics" },
+                    { 3, "Toys" },
+                    { 4, "Sports" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "ProductCategories",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "ProductCategories",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "ProductCategories",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "ProductCategories",
+                keyColumn: "Id",
+                keyValue: 4);
+        }
+    }
+}
