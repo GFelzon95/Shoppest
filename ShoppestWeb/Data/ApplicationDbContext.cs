b73903a01c5e81c4908338075bc4c31892639692
Gian Felzon Pascual <pascual.gianfelzon@wesleyan.edu.ph>
2024-02-01 22:27:55 +0800

    -Setup Connection String, ApplicationDbContext, Database

---
+++ ShoppestWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ShoppestWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
