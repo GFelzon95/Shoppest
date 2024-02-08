2d0eaafc433550da50c9aa6c97a4dce95a4820f5
Gian Felzon Pascual <pascual.gianfelzon@wesleyan.edu.ph>
2024-02-08 20:30:58 +0800

    Implemented Areas(Admin and Customer),  changed Bootstrap Theme, implemented Repository and Unit of Work to Category, combined Create and Edit actions into Upsert in Category controller i

--- Shoppest.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ Shoppest.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -2,7 +2,7 @@
 {
     public interface IUnitOfWork
     {
-        IProductCategoryRepository ProductCategory { get; }
+        IProductCategoryRepository ProductCategories { get; }
 
         void Save();
     }
