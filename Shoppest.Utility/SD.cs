b87b804d004f474cb157e51e7171772afa2beac6
Gian Felzon Pascual <pascual.gianfelzon@wesleyan.edu.ph>
2024-02-25 09:41:53 +0800

    Created Roles In Database

---
+++ Shoppest.Utility/SD.cs
@@ -0,0 +1,9 @@
+﻿namespace Shoppest.Utility
+{
+    public class SD
+    {
+        public const string Role_Customer = "Customer";
+        public const string Role_Admin = "Admin";
+        public const string Role_Employee = "Employee";
+    }
+}
