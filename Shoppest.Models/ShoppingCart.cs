f9d1764a75e34640df15eb1eeb957cf5ffb812f4
Gian Felzon Pascual <pascual.gianfelzon@wesleyan.edu.ph>
2024-02-27 17:14:40 +0800

    Create Shopping Cart Model, Add Shopping Cart Table to Db

---
+++ Shoppest.Models/ShoppingCart.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Shoppest.Models
+{
+    public class ShoppingCart
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        [ForeignKey("ProductId")]
+        [ValidateNever]
+        public Product Product { get; set; }
+        public int Count { get; set; }
+        public string ApplicationUserId { get; set; }
+        [ForeignKey("ApplicationUserId")]
+        [ValidateNever]
+        public ApplicationUser ApplicationUser { get; set; }
+    }
+}
