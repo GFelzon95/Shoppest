6be0336e5b20392bfbc467b140a1c3b9a591452a
Gian Felzon Pascual <pascual.gianfelzon@wesleyan.edu.ph>
2024-03-06 19:09:32 +0800

    Add OrderHeader and OrderDetails Models and Table to Db

--- Shoppest.DataAccess/Data/ApplicationDbContext.cs
+++ Shoppest.DataAccess/Data/ApplicationDbContext.cs
@@ -4,73 +4,75 @@ using Shoppest.Models;
 
 namespace Shoppest.DataAccess
 {
-    public class ApplicationDbContext : IdentityDbContext
-    {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
-        {
+	public class ApplicationDbContext : IdentityDbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+		{
 
-        }
+		}
 
-        public DbSet<ProductCategory> ProductCategories { get; set; }
-        public DbSet<Product> Products { get; set; }
-        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
-        public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+		public DbSet<ProductCategory> ProductCategories { get; set; }
+		public DbSet<Product> Products { get; set; }
+		public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+		public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+		public DbSet<OrderHeader> OrderHeaders { get; set; }
+		public DbSet<OrderDetail> OrderDetails { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
 
-            modelBuilder.Entity<ProductCategory>().HasData(
-                new ProductCategory { Id = 1, Name = "Fashion" },
-                new ProductCategory { Id = 2, Name = "Electronics" },
-                new ProductCategory { Id = 3, Name = "Toys" },
-                new ProductCategory { Id = 4, Name = "Sports" }
-                );
+			modelBuilder.Entity<ProductCategory>().HasData(
+				new ProductCategory { Id = 1, Name = "Fashion" },
+				new ProductCategory { Id = 2, Name = "Electronics" },
+				new ProductCategory { Id = 3, Name = "Toys" },
+				new ProductCategory { Id = 4, Name = "Sports" }
+				);
 
-            modelBuilder.Entity<Product>().HasData(
-                new Product
-                {
-                    Id = 1,
-                    Name = "Denim Jeans",
-                    PictureUrl = "",
-                    Description = "Jeans with no holes in it.",
-                    Price = 30,
-                    Quantity = 100,
-                    ProductCategoryId = 1
-                },
-                new Product
-                {
-                    Id = 2,
-                    Name = "PS6",
-                    PictureUrl = "",
-                    Description = "New Play Station with so advanced features that it's not even released yet.",
-                    Price = 1200,
-                    Quantity = 0,
-                    ProductCategoryId = 2
-                },
-                new Product
-                {
-                    Id = 3,
-                    Name = "Yo-Yo",
-                    PictureUrl = "",
-                    Description = "It's just a Yo-Yo.",
-                    Price = 5,
-                    Quantity = 200,
-                    ProductCategoryId = 3
-                },
-                new Product
-                {
-                    Id = 4,
-                    Name = "Soccer Shoes",
-                    PictureUrl = "",
-                    Description = "Latest pair of soccess shoes with advanced technology... But it's a pair of right foot shoes only.",
-                    Price = 100,
-                    Quantity = 20,
-                    ProductCategoryId = 4
-                }
-                );
-        }
+			modelBuilder.Entity<Product>().HasData(
+				new Product
+				{
+					Id = 1,
+					Name = "Denim Jeans",
+					PictureUrl = "",
+					Description = "Jeans with no holes in it.",
+					Price = 30,
+					Quantity = 100,
+					ProductCategoryId = 1
+				},
+				new Product
+				{
+					Id = 2,
+					Name = "PS6",
+					PictureUrl = "",
+					Description = "New Play Station with so advanced features that it's not even released yet.",
+					Price = 1200,
+					Quantity = 0,
+					ProductCategoryId = 2
+				},
+				new Product
+				{
+					Id = 3,
+					Name = "Yo-Yo",
+					PictureUrl = "",
+					Description = "It's just a Yo-Yo.",
+					Price = 5,
+					Quantity = 200,
+					ProductCategoryId = 3
+				},
+				new Product
+				{
+					Id = 4,
+					Name = "Soccer Shoes",
+					PictureUrl = "",
+					Description = "Latest pair of soccess shoes with advanced technology... But it's a pair of right foot shoes only.",
+					Price = 100,
+					Quantity = 20,
+					ProductCategoryId = 4
+				}
+				);
+		}
 
 
-    }
+	}
 }
---
+++ Shoppest.Models/OrderHeader.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Shoppest.Models
+{
+	public class OrderHeader
+	{
+		public int Id { get; set; }
+		public string ApplicationUserId { get; set; }
+		[ForeignKey(nameof(ApplicationUserId))]
+		[ValidateNever]
+		public ApplicationUser ApplicationUser { get; set; }
+
+		public DateTime OrderDate { get; set; }
+		public DateTime ShippingDate { get; set; }
+		public double OrderTotal { get; set; }
+
+		public string? OrderStatus { get; set; }
+		public string? PaymentStatus { get; set; }
+		public string? TrackingNumber { get; set; }
+		public string? Carrier { get; set; }
+
+		public DateTime PaymentDate { get; set; }
+		public DateOnly PaymentDueDate { get; set; }
+
+		public string? PaymentIntentId { get; set; }
+
+		[Required]
+		public string Name { get; set; }
+		[Required]
+		public string PhoneNumber { get; set; }
+		[Required]
+		public string Region { get; set; }
+		[Required]
+		public string Province { get; set; }
+		[Required]
+		public string City { get; set; }
+		[Required]
+		public string Barangay { get; set; }
+		[Required]
+		public string StreetAddress { get; set; }
+		[Required]
+		public string PostalCode { get; set; }
+	}
+}
