88555de713289e6e11f7feb4d281782a5369f73c
Gian Felzon Pascual <pascual.gianfelzon@wesleyan.edu.ph>
2024-03-07 21:16:09 +0800

    Configure Stripe in Project

---
+++ Shoppest.Utility/StripeSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace Shoppest.Utility
+{
+    public class StripeSettings
+    {
+        public string PublishableKey { get; set; }
+        public string SecretKey { get; set; }
+    }
+}
--- ShoppestWeb/Program.cs
+++ ShoppestWeb/Program.cs
@@ -5,6 +5,7 @@ using Shoppest.DataAccess;
 using Shoppest.DataAccess.Repository;
 using Shoppest.DataAccess.Repository.IRepository;
 using Shoppest.Utility;
+using Stripe;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +13,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.Configure<StripeSettings>(builder.Configuration.GetSection("Stripe"));
 
 builder.Services.AddIdentity<IdentityUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = false).AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
 builder.Services.ConfigureApplicationCookie(options =>
@@ -36,7 +39,7 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
+StripeConfiguration.ApiKey = builder.Configuration.GetSection("Stripe:SecretKey").Get<string>();
 app.UseRouting();
 
 app.UseAuthorization();
