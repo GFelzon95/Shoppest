34695beacc4d9297a04b019ff246c2372adeb4b5
Gian Felzon Pascual <pascual.gianfelzon@wesleyan.edu.ph>
2024-03-12 23:22:37 +0800

    Add SessionId to OrderHeader, Add Summary UI with empty Cart

--- Shoppest.Models/OrderHeader.cs
+++ Shoppest.Models/OrderHeader.cs
@@ -25,6 +25,7 @@ namespace Shoppest.Models
         public DateTime PaymentDate { get; set; }
         public DateOnly PaymentDueDate { get; set; }
 
+        public string? SessionId { get; set; }
         public string? PaymentIntentId { get; set; }
 
         [Required]
