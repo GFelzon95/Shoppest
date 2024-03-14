f05aa062bced37ea668b8ce77f0f8af937488d6c
Gian Felzon Pascual <pascual.gianfelzon@wesleyan.edu.ph>
2024-03-14 21:35:06 +0800

    Add Helper Methods in Order Header Repository

--- Shoppest.DataAccess/Repository/OrderHeaderRepository.cs
+++ Shoppest.DataAccess/Repository/OrderHeaderRepository.cs
@@ -3,17 +3,44 @@ using Shoppest.Models;
 
 namespace Shoppest.DataAccess.Repository
 {
-	public class OrderHeaderRepository : Repository<OrderHeader>, IOrderHeaderRepository
-	{
-		private readonly ApplicationDbContext _context;
-		public OrderHeaderRepository(ApplicationDbContext context) : base(context)
-		{
-			_context = context;
-		}
+    public class OrderHeaderRepository : Repository<OrderHeader>, IOrderHeaderRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public OrderHeaderRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
 
-		public void Update(OrderHeader orderHeader)
-		{
-			_context.OrderHeaders.Update(orderHeader);
-		}
-	}
+        public void Update(OrderHeader orderHeader)
+        {
+            _context.OrderHeaders.Update(orderHeader);
+        }
+
+        public void UpdateStatus(int id, string orderStatus, string? paymentStatus = null)
+        {
+            var orderFromDb = _context.OrderHeaders.FirstOrDefault(o => o.Id == id);
+            if (orderFromDb != null)
+            {
+                orderFromDb.OrderStatus = orderStatus;
+                if (!string.IsNullOrEmpty(paymentStatus))
+                {
+                    orderFromDb.PaymentStatus = paymentStatus;
+                }
+            }
+        }
+
+        public void UpdateStripePaymentID(int id, string sessionId, string paymentIntentId)
+        {
+            var orderFromDb = _context.OrderHeaders.FirstOrDefault(o => o.Id == id);
+            if (!string.IsNullOrEmpty(sessionId))
+            {
+                orderFromDb.SessionId = sessionId;
+            }
+            if (!string.IsNullOrEmpty(paymentIntentId))
+            {
+                orderFromDb.PaymentIntentId = paymentIntentId;
+                orderFromDb.PaymentDate = DateTime.Now;
+            }
+        }
+    }
 }
