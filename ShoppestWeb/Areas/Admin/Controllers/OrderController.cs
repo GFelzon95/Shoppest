f169b8a99f948e2b9c1099c6e48631b71e5f0344
Gian Felzon Pascual <pascual.gianfelzon@wesleyan.edu.ph>
2024-03-18 15:13:11 +0800

    Add Order Index UI

---
+++ ShoppestWeb/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Shoppest.DataAccess.Repository.IRepository;
+using Shoppest.Models;
+using Shoppest.Utility;
+using System.Security.Claims;
+
+namespace ShoppestWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class OrderController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API CALLS
+
+        [HttpGet]
+        [Authorize]
+        public IActionResult GetAll()
+        {
+            IEnumerable<OrderHeader> orderHeaders;
+
+            if (User.IsInRole(SD.Role_Admin) || User.IsInRole(SD.Role_Employee))
+            {
+                orderHeaders = _unitOfWork.OrderHeaders.GetAll(includeProperties: "ApplicationUser");
+            }
+            else
+            {
+                var claimsIdentity = (ClaimsIdentity)User.Identity;
+                var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+                orderHeaders = _unitOfWork.OrderHeaders.GetAll(o => o.ApplicationUserId == userId);
+            }
+
+            return Json(new { data = orderHeaders });
+        }
+
+        #endregion
+    }
+}
---
+++ ShoppestWeb/wwwroot/js/order.js
@@ -0,0 +1,34 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+})
+
+function loadDataTable() {
+    dataTable = $("#orderTbl").DataTable({
+        "ajax": { url: '/admin/order/getall' },
+        "columns": [
+            { data: "id", width: "5%" },
+            { data: "name", width: "25%" },
+            { data: "phoneNumber", width: "15%" },
+            { data: "applicationUser.email", width: "25%" },
+            { data: "orderStatus", width: "10%" },
+            {
+                data: "orderTotal",
+                render: function (data) {
+                    return data + "$";
+                },
+                width: "10%"
+            },
+            {
+                data: "id",
+                render: function (data) {
+                    return `<div class="text-center">
+                            <a href="/Admin/Order/Details?id=${data}" class="btn btn-primary"><i class="bi bi-pencil-square"></i></a>
+                        </div>`
+                },
+                width:"10%"
+            }
+        ]
+    })
+}
